293d230b2339d9ba42660f083506fae0d7b2d9ce
Peter Bryant <peter.bryant@netsells.co.uk>
2021-09-21 12:47:51 +0100

    ✅ Add test for LoggingBlocObserver

---
+++ compose/src/test/java/com/ptrbrynt/kotlin_bloc/compose/LoggingBlocObserverTest.kt
@@ -0,0 +1,45 @@
+package com.ptrbrynt.kotlin_bloc.compose
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.ptrbrynt.kotlin_bloc.compose.blocs.CounterBloc
+import com.ptrbrynt.kotlin_bloc.compose.blocs.CounterEvent
+import com.ptrbrynt.kotlin_bloc.core.Bloc
+import io.mockk.mockk
+import io.mockk.verifyOrder
+import kotlinx.coroutines.FlowPreview
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+import org.robolectric.shadows.ShadowLog
+import java.io.PrintStream
+
+@FlowPreview
+@RunWith(AndroidJUnit4::class)
+@Config(shadows = [ShadowLog::class])
+class LoggingBlocObserverTest {
+
+    @get:Rule
+    val composableTestRule = createComposeRule()
+
+    @Test
+    fun `LoggingBlocObserver prints correct data`() {
+        val stream = mockk<PrintStream>(relaxed = true)
+
+        ShadowLog.stream = stream
+
+        Bloc.observer = LoggingBlocObserver()
+
+        val bloc = CounterBloc()
+
+        bloc.add(CounterEvent.Increment)
+
+        verifyOrder {
+            stream.println("I/CounterBloc: Created")
+            stream.println("I/CounterBloc: Increment")
+            stream.println("I/CounterBloc: Transition(state=0, event=Increment, newState=1)")
+            stream.println("I/CounterBloc: Change(state=0, newState=1)")
+        }
+    }
+}
